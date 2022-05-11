ba8dceec48d68ce572ee8c845f29602bd8dc2c89
osakepanda <yonayona.kyoto@gmail.com>
2022-05-11 15:46:13 +0900

    delete slideshow.css

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
-import '../styles/slideshow.css'
 
 const properties = {
   duration: 5000,                // 間隔(ms)
