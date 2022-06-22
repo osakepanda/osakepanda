8cfd9b7358636db4506e8bc3f44f39580fb26d5f
w-higa <www.higa@gmail.com>
2022-06-22 15:55:33 +0900

    SlideShowを修正

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -17,20 +17,27 @@ const properties = {
 
 const SlideShow = ({ items }) => {
   return (
-    <div className="App">
-      <div className="slide-container">
-        <Slide {...properties}>
-          {items.map((item, index) => {
-              return (
-                <div className="each-slide" key={item}>
-                  <div key={index} style={{ backgroundImage: `url(${item.image.url})` }} />
-                </div>
-              )
-            }
-          )}
-        </Slide>
-      </div>
+    <div className="slide-container">
+      <Slide {...properties}>
+        {items.map((each, index) => (
+          <img key={index} style={{ width: "100%" }} src={each} />
+        ))}
+      </Slide>
     </div>
+    // <div className="App">
+    //   <div className="slide-container">
+    //     <Slide {...properties}>
+    //       {items.map((item, index) => {
+    //           return (
+    //             <div className="each-slide" key={item}>
+    //               <div key={index} style={{ backgroundImage: `url(${item.image.url})` }} />
+    //             </div>
+    //           )
+    //         }
+    //       )}
+    //     </Slide>
+    //   </div>
+    // </div>
   )
 }
 
